a00c047426b955a731ee34aedb7b2f49dda04592
Hanssen <35262249+hanssenstijn@users.noreply.github.com>
2020-01-20 21:19:57 +0100

    Update script.js
    - using functions
    - loops

--- JS/script.js
+++ JS/script.js
@@ -14,7 +14,7 @@ function sing(song) {
     console.log(song);
 }
 
-sing("la la");
+sing("La la");
 sing("Yo yo");
 
 // a and b are here the parameters
@@ -26,4 +26,134 @@ function multiply(a,b) {
 }
 // 2 and 4 are arguments
 multiply(2,4);
+
+// array
+var list = ["tiger","cat","bear","bird"];
+console.log(list[1]);
+
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+// remove first item
+console.log(array.shift());
+// sort array
+console.log(array.sort())
+// add kiwi at the end
+console.log(array.concat(["Kiwi"]))
+
+// object
+var user = {
+    name: "john",
+    age: 34,
+    hobby: "soccer",
+    isMarried: false,
+    spells: ["abrakadabra","boom"]
+};
+// asses an atribute
+console.log(user.age);
+// add infromation
+user.favouriteFood = "spinach";
+console.log(user.favouriteFood);
+
+var database = [
+    {
+        username: "stijn",
+        password: "secret"
+    },
+    {
+        username: "hans",
+        password: "123"
+    },
+    {
+        username: "jan",
+        password: "777"
+    },
+];
+
+var newFeed = [
+    {
+        username: "bob",
+        timeline: "tired"
+    },
+    {
+        username: "sal",
+        timeline: "JS"
+    }
+];
+
+var usernamePrompt = prompt("your username");
+var passwordPrompt = prompt("your password");
+
+function isUserValid(user, pass) {
+    for (var i=0; i < database.length; i++) {
+        if (database[i].username === user && database[i].password === pass) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function signIn(user, pass) {
+    console.log(isUserValid(user,pass));
+    // if (user === database[0].username && pass === database[0].password) {
+    //     console.log(newFeed);
+    // } else {
+    //     alert("wrong")
+    // }
+}
+
+signIn(usernamePrompt,passwordPrompt);
+
+function newfunction(){
+    console.log("yo")
+}
+// this example need a semicolon at the end
+var newfunction = function() {
+
+};
+
+// expresion
+1+3;
+var a =2;
+
+// differene function and method
+function thisisafunction(){
+
+};
+
+var obj = {
+    thisisamethod: function () {
+
+    }
+};
+
+// difference in assesing
+thisisafunction();
+// using the dot (.) for the method
+obj.thisisamethod();
+
+//for loop
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exerise",
+    "study JS",
+    "eat healthy"
+];
+var todoslength = todos.length;
+for (var i=0; i < todoslength; i++) {
+    todos.pop();
+}
+
+// while loop
+var counterOne = 0;
+while(counterOne < 4){
+    console.log(counterOne);
+    counterOne++
+}
+
+// do
+var countertwo = 10;
+do {
+    console.log(countertwo);
+    countertwo--;
+} while (countertwo > 10);
 
